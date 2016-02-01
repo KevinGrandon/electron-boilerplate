f90951d96072dac04ccdad8e33e90e6c214b2cb6
Kevin Grandon <kevingrandon@yahoo.com>
2016-01-31 19:29:26 -0800

    Add a script for building for production.

--- config/webpack.development.config.js
+++ config/webpack.development.config.js
@@ -3,31 +3,31 @@ var webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
 
 var config = {
   entry: {
-  app: ['webpack/hot/dev-server', './src/components/index.js'],
-},
+    app: ['webpack/hot/dev-server', './src/components/index.js'],
+  },
 
-output: {
-  path: './public/built',
-  filename: 'bundle.js',
-  publicPath: 'http://localhost:8080/built/'
-},
+  output: {
+    path: './public/built',
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:8080/built/'
+  },
 
-devServer: {
-  contentBase: './public',
-  publicPath: 'http://localhost:8080/built/'
-},
+  devServer: {
+    contentBase: './public',
+    publicPath: 'http://localhost:8080/built/'
+  },
 
-module: {
- loaders: [
-   { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-   { test: /\.css$/, loader: 'style-loader!css-loader' },
-   { test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}
- ]
-},
+  module: {
+   loaders: [
+     { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+     { test: /\.css$/, loader: 'style-loader!css-loader' },
+     { test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}
+   ]
+  },
 
- plugins: [
-   new webpack.HotModuleReplacementPlugin()
- ]
+   plugins: [
+     new webpack.HotModuleReplacementPlugin()
+   ]
 }
 
 config.target = webpackTargetElectronRenderer(config)
--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -3,31 +3,22 @@ var webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
 
 var config = {
   entry: {
-  app: ['webpack/hot/dev-server', './src/components/index.js'],
-},
+    app: ['./src/components/index.js'],
+  },
 
-output: {
-  path: './public/built',
-  filename: 'bundle.js',
-  publicPath: 'http://localhost:8080/built/'
-},
+  output: {
+    path: './public/built',
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:8080/built/'
+  },
 
-devServer: {
-  contentBase: './public',
-  publicPath: 'http://localhost:8080/built/'
-},
-
-module: {
- loaders: [
-   { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-   { test: /\.css$/, loader: 'style-loader!css-loader' },
-   { test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}
- ]
-},
-
- plugins: [
-   new webpack.HotModuleReplacementPlugin()
- ]
+  module: {
+   loaders: [
+     { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+     { test: /\.css$/, loader: 'style-loader!css-loader' },
+     { test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}
+   ]
+  }
 }
 
 config.target = webpackTargetElectronRenderer(config)
